0fb1e93b4583a0bd6cfa2e684376572e20ba869d
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2019-05-19 22:27:50 +0200

    Ajoute test_glouton.py

---
+++ test/test_glouton.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright https://github.com/VPerrollaz
+#
+# Distributed under terms of the %LICENSE% license.
+
+"""
+Test de l'algorithme glouton.
+"""
+from src.graphe import Graphe
+from src.glouton import glouton
+
+
+def test_glouton_simple():
+    """Test de l'algorithme glouton sur le graphe le plus simple."""
+    graphe = Graphe(2)
+    assert glouton(graphe) == [1, 2]
