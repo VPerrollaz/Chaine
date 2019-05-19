08d18903c88b0364e98927b075e202942409bfac
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2019-05-19 22:07:35 +0200

    Initialise et représente Graphe.

--- src/graphe.py
+++ src/graphe.py
@@ -10,3 +10,23 @@
 """
 Génération du graphe permettant de coder un algorithme glouton.
 """
+
+
+class Graphe:
+    def __init__(self, N):
+        self.nb_max = N
+        self.demarrage = list(range(1, N + 1))
+        self.voisins = dict()
+        for i in range(1, N + 1):
+            self.voisins[i] = list()
+            for j in range(1, N + 1):
+                if i == j:
+                    continue
+                if (i % j == 0) or (j % i == 0):
+                    self.voisins[i].append(j)
+
+    def __repr__(self):
+        return f"Graphe({self.nb_max})"
+
+    def __str__(self):
+        return "{}\n{}".format(self.demarrage, self.voisins)
