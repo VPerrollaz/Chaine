c9208867200b8a9be1df87531f02c820d4fff4af
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2019-05-19 21:59:46 +0200

    Ajoute de graphe.py

---
+++ src/graphe.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright https://github.com/VPerrollaz
+#
+# Distributed under terms of the %LICENSE% license.
+
+"""
+Génération du graphe permettant de coder un algorithme glouton.
+"""
