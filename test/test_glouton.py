517240731f9fac68b45a505965ed75dbd806478c
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2019-05-19 22:39:29 +0200

    Ajoute un test glouton moins simple

--- test/test_glouton.py
+++ test/test_glouton.py
@@ -18,3 +18,9 @@ def test_glouton_simple():
     """Test de l'algorithme glouton sur le graphe le plus simple."""
     graphe = Graphe(2)
     assert glouton(graphe) == [1, 2]
+
+
+def test_glouton():
+    """Cas moins simple"""
+    graphe = Graphe(10)
+    assert glouton(graphe) == [1, 2, 4, 8]
