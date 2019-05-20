8f9bbf77499ab60f729bf77f2614293417cb645e
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2019-05-20 22:40:57 +0200

    Implémente le recuit avec un itérateur de température en entrée

--- src/recuit.py
+++ src/recuit.py
@@ -11,6 +11,7 @@
 Implémentation de l'algorithme de recuit simulé.
 """
 from math import exp
+import random as rd
 from src.graphe import Graphe
 from src.glouton import glouton
 
@@ -23,3 +24,15 @@ def auxiliaire(nombre: float) -> float:
 def energie(graphe: Graphe, temp: float) -> float:
     """Calcul de l'énergie d'un graphe pour un niveau de température donné."""
     return exp(len(glouton(graphe)) / temp)
+
+
+def recuit(nb_max, temperature):
+    """Implémente le recuit pour l'itérateur de température donné"""
+    graphe = Graphe.default(nb_max)
+    for temp in temperature:
+        en1 = energie(graphe, temp)
+        graphe.mutation()
+        en2 = energie(graphe, temp)
+        if rd.random() > auxiliaire(en2 / en1):
+            graphe.inversion()
+    return graphe
