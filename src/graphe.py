f79d8de63fd193d3f36d1b7532b434dc3772e500
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2019-05-20 21:52:43 +0200

    Modifie le constructeur explicite/ ordre croissant.

--- src/graphe.py
+++ src/graphe.py
@@ -40,37 +40,43 @@ class Mouvement:
 class Graphe:
     """Classe permettant la paramétrisation d'un algorithme glouton."""
 
-    def __init__(self, N):
-        self.nb_max = N
-        self.demarrage = list(range(1, N + 1))
-        self.voisins = dict()
-        for i in range(1, N + 1):
-            self.voisins[i] = list()
-            for j in range(1, N + 1):
+    def __init__(self, demarrage, voisinage):
+        self.demarrage = demarrage
+        self.voisinage = voisinage
+        self.historique = list()
+
+    @classmethod
+    def default(cls, nb_sommets):
+        """Initialisation par ordre croissant."""
+        demarrage = list(range(1, nb_sommets + 1))
+        voisinage = dict()
+        for i in range(1, nb_sommets + 1):
+            voisinage[i] = list()
+            for j in range(1, nb_sommets + 1):
                 if i == j:
                     continue
                 if (i % j == 0) or (j % i == 0):
-                    self.voisins[i].append(j)
-        self.historique = list()
+                    voisinage[i].append(j)
+        return cls(demarrage, voisinage)
 
     def __repr__(self):
-        return f"Graphe({self.nb_max})"
+        return f"Graphe({self.demarrage, self.voisinage})"
 
     def __str__(self):
-        return "{}\n{}".format(self.demarrage, self.voisins)
+        return "{}\n{}".format(self.demarrage, self.voisinage)
 
     def modification(self, mouvement: Mouvement):
         """Modifie le graphe en fonction du mouvement demandé."""
         entier1, entier2 = mouvement.demarrage
         echange(self.demarrage, entier1, entier2)
         entier, voisin1, voisin2 = mouvement.voisinage
-        echange(self.voisins[entier], voisin1, voisin2)
+        echange(self.voisinage[entier], voisin1, voisin2)
 
     def mutation(self):
         """Détermine une transition possible et l'ajoute à l'historique."""
         entier1, entier2 = rd.sample(self.demarrage, 2)
         entier = rd.choice(self.demarrage)
-        voisin1, voisin2 = rd.sample(self.voisins[entier], 2)
+        voisin1, voisin2 = rd.sample(self.voisinage[entier], 2)
         mouv = Mouvement((entier1, entier2), (entier, voisin1, voisin2))
         self.historique.append(mouv)
         self.modification(mouv)
