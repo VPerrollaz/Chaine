d7109207c0e8394707ebd800940e9c2d4a470128
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2019-05-20 20:34:00 +0200

    Ajoute la fonction energie dans recuit.py

--- src/recuit.py
+++ src/recuit.py
@@ -10,8 +10,16 @@
 """
 Implémentation de l'algorithme de recuit simulé.
 """
+from math import exp
+from src.graphe import Graphe
+from src.glouton import glouton
 
 
-def h(u):
+def auxiliaire(nombre: float) -> float:
     """Fonction auxiliaire de l'algorithme de recuit."""
-    return u / (1 + u)
+    return nombre / (1 + nombre)
+
+
+def energie(graphe: Graphe, temp: float) -> float:
+    """Calcul de l'énergie d'un graphe pour un niveau de température donné."""
+    return exp(-len(glouton(graphe)) / temp)
