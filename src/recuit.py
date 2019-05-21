8e0ed5c46a563f11c52d83ab27c9646d6e10c58c
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2019-05-21 20:51:35 +0200

    Enleve debug, stocke la plus longue chaine, et accepte tous les nouveaux records

--- src/recuit.py
+++ src/recuit.py
@@ -21,23 +21,24 @@ def auxiliaire(nombre: float) -> float:
     return nombre / (1 + nombre)
 
 
-def energie(graphe: Graphe, temp: float) -> float:
+def energie(chaine: list, temp: float) -> float:
     """Calcul de l'énergie d'un graphe pour un niveau de température donné."""
-    return exp(len(glouton(graphe)) / temp)
+    return exp(len(chaine) / temp)
 
 
-def recuit(nb_max, temperature, debug=False):
+def recuit(nb_max, temperature):
     """Implémente le recuit pour l'itérateur de température donné"""
     graphe = Graphe.default(nb_max)
+    meilleure = list()
     for temp in temperature:
-        en1 = energie(graphe, temp)
+        ch1 = glouton(graphe)
         graphe.mutation()
-        en2 = energie(graphe, temp)
-        if rd.random() > auxiliaire(en2 / en1):
+        ch2 = glouton(graphe)
+        if len(ch2) > len(meilleure):
+            meilleure = ch2
+        elif rd.random() > auxiliaire(energie(ch2, temp) / energie(ch1, temp)):
             graphe.inversion()
-        elif debug:
-            print(len(glouton(graphe)), end=", ")
-    return graphe
+    return graphe, meilleure
 
 
 def temp_log(nb_iterations, cte=1.0):
